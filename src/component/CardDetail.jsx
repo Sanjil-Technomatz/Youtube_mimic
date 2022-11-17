bc20a6ca08492fa9b9b149c0929d41d3c5ac0461
sanjil kaushal <skaushal@technomatz.com>
2022-11-17 16:03:14 +0530

    add tooltip for channel name

--- src/component/CardDetail.jsx
+++ src/component/CardDetail.jsx
@@ -1,40 +1,51 @@
-import { useState } from "react"
-import CardModal from './CardModal'
+import { useState } from "react";
+import CardModal from "./CardModal";
 import { getTimeOfPublish } from "../util";
-import './common.css'
+import "./common.css";
 const CardDetail = ({ id, imgurl, title, channelName, time }) => {
-
   const [hoverState, setHoverState] = useState(false);
+  const [tooltip, setTooltip] = useState(false);
 
   return (
     // <>{hoverState && <CardModal ismodalOpen={hoverState} onclose={() => { setHoverState(false) }} imgurl={imgurl} title={title} channelName={channelName} time={time} />}
     <>
-      <div className={hoverState ? "card cardZoom" : 'card'}>
-        <div onMouseOver={() => setHoverState(true)} onMouseOut={() => setHoverState(false)} >
+      <div className={hoverState ? "card cardZoom" : "card"}>
+        <div
+          onMouseOver={() => setHoverState(true)}
+          onMouseOut={() => setHoverState(false)}
+        >
           <img src={imgurl} alt={id} className="imageThumb" />
         </div>
         <div>
-          <span style={{ fontWeight: '600', fontSize: '16px' }}>
-            {title}
-          </span>
-          <div style={{ fontWeight: '400', fontSize: '12px', color: '#888' }}>
+          <span style={{ fontWeight: "600", fontSize: "14px" }}>{title}</span>
+          {tooltip && <div className="tooltip">{channelName}</div>}
+          1<br />
+          <span
+            style={{ fontWeight: "500", fontSize: "14px", color: "#606060" }}
+            onMouseOver={() => {
+              setTooltip(true);
+            }}
+            onMouseOut={() => {
+              setTooltip(false);
+            }}
+          >
             {channelName}
-          </div>
-          <div style={{ fontWeight: '400', fontSize: '12px', color: '#888' }}>
+          </span>
+          <div
+            style={{ fontWeight: "400", fontSize: "15px", color: "#606060" }}
+          >
             {getTimeOfPublish(time)}
           </div>
           {hoverState && (
             <div className="buttonContainer">
               <button>Whatch Letter </button>
               <button>Add to playlist</button>
-            </div>)
-          }
+            </div>
+          )}
         </div>
       </div>
-
-
     </>
   );
-}
+};
 
 export default CardDetail;
--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-export const BaseUrl = 'https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=50&q=programming&key=AIzaSyAkimHME9AFzozQeEAFpSHgSRGQeqqIG9E'
+export const BaseUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=50&q=programming&key=${process.env.REACT_APP_API_KEY}`;
